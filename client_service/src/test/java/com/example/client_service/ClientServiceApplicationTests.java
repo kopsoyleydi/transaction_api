b7947e669530c003dd2534cf6fe6ec995693aa19
nobeksultan <kuralbaibexultan@gmail.com>
2024-04-16 21:39:19 +0500

    init all services

---
+++ client_service/src/test/java/com/example/client_service/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.client_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ transaction_service/src/main/java/com/example/transaction_service/model/Transaction.java
@@ -0,0 +1,5 @@
+package com.example.transaction_service.model;
+
+
+public class Transaction {
+}
